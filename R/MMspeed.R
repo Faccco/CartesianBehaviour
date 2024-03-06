de4d888e79191e147e80662f196400607921bfba
Faccco <falcoga93@gmail.com>
2024-03-06 12:24:29 -0300

    Fix expdados

--- R/MMspeed.R
+++ R/MMspeed.R
@@ -24,7 +24,7 @@
 #' rm(Z1, Z2, Z3, Z4, ZN)
 #'
 #' #Perform all the measures
-#' TRAJ2D <- Allbasics(Traj, Xaxi = "x", Yaxi = "y", frames = "time",
+#' TRAJ2D <- Allbasics(Traj, Xaxi = "x", Yaxi = "y", time = "time",
 #'                     Zones = Zonas, threshold = 3,
 #'                     Dist.reg = "point",
 #'                     reg = data.frame(x = 0, y = 0))
